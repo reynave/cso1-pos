2708875827d68029fb4d1b927e261e31a21c41b6
reynave <felix.renaldi@gmail.com>
2023-11-02 12:38:36 +0700

    add DB

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,12 +7,13 @@ class Home extends BaseController
 {
     public function index()
     {
-        $q1 = "SELECT *  FROM auto_number";
+        $q1 = "SELECT *  FROM auto_number limit 1";
         $items = $this->db->query($q1)->getResultArray();
 
         $data = array(
             "error" => false,
-            "items" => $items,
+            "con" => 200,
+            "items" => $items
         );
         return $this->response->setJSON($data);
     }
